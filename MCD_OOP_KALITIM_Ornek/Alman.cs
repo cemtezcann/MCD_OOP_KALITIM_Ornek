7bc418d32123123160ebea2aaccdd74414baa108
cmtzc <tezcancem92@gmail.com>
2021-12-03 16:14:21 +0300

    Add project files.

---
+++ MCD_OOP_KALITIM_Ornek/Alman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOP_KALITIM_Ornek
+{
+    
+        public class Alman : Insan
+        {
+
+            public override void Selamlasma()
+            {
+                Console.WriteLine("Hallo");
+            }
+        }
+    
+}
